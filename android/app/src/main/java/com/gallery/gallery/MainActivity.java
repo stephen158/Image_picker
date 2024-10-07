c8c7510edd2362c2d6e6c4b970ad23f2f520ec18
stephen158 <arockstephen.developer@gmail.com>
2024-10-07 09:53:40 +0530

    Image picker with permission

---
+++ android/app/src/main/java/com/gallery/gallery/MainActivity.java
@@ -0,0 +1,6 @@
+package com.gallery.gallery;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+   File? _image;
+  final imagePicker=ImagePicker();
+
+  Future getImage() async{
+    final image = await imagePicker.pickImage(source : ImageSource.camera);
+    setState(() {
+      _image=File(image!.path);
+    });
+  }
+  Future _getImageFromGallery() async{
+    final image = await imagePicker.pickImage(source : ImageSource.gallery);
+    setState(() {
+      _image=File(image!.path);
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+       backgroundColor: Colors.black,
+        title: Text('Gallery',style: TextStyle(color: Colors.blue),),
+        leading: Icon(Icons.menu,color: Colors.white,),
+      ),
+      body:Center(
+           child: Column(
+             children: [
+               Center(child: _image == null ? Text('No Image Selected') : Image.file(_image!)),
+               const SizedBox(height: 100),
+               ElevatedButton(
+                 style: ElevatedButton.styleFrom(
+                   backgroundColor: Colors.lightBlueAccent,
+                 ),
+                   onPressed: (){
+                       _getImageFromGallery();
+                   }, child: Text('Gallery',
+                 style: TextStyle(color: Colors.white,
+                     fontWeight: FontWeight.bold,
+                     fontSize:20),),),
+               ],
+           ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: ()async{
+            PermissionStatus camerastatus=  await Permission.camera.request();
+            if(camerastatus ==PermissionStatus.granted){
+              getImage();
+            }
+            if(camerastatus == PermissionStatus.denied){
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("you need to provide camera Permission")));
+            }
+            if(camerastatus == PermissionStatus.permanentlyDenied){
+              openAppSettings();
+            }
+          },backgroundColor: Colors.blue,
+        child: Icon(Icons.camera_alt,color: Colors.black,
+        )
+      ),
+    );
+  }
+}
+
+
